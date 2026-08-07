9788d36f4d25d3a07d7ef3874f5737dd6dd7f667
camgianela09 <cam.gianela09@gmail.com>
2026-08-07 13:04:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
 const cursos = document.querySelectorAll(".curso");
 
 
-cursos.forEach(curso=>{
+// Recuperar cursos guardados
+cursos.forEach((curso, index) => {
 
-curso.onclick=function(){
+    let estado = localStorage.getItem("curso" + index);
 
-this.classList.toggle("seleccionado");
+    if(estado){
+        curso.classList.add(estado);
+    }
 
-}
+
+    curso.onclick = function(){
+
+        this.classList.remove(
+            "aprobado",
+            "cursando",
+            "pendiente"
+        );
+
+
+        if(!this.dataset.estado){
+            this.dataset.estado="aprobado";
+        }
+        else if(this.dataset.estado==="aprobado"){
+            this.dataset.estado="cursando";
+        }
+        else if(this.dataset.estado==="cursando"){
+            this.dataset.estado="pendiente";
+        }
+        else{
+            this.dataset.estado="aprobado";
+        }
+
+
+        this.classList.add(this.dataset.estado);
+
+
+        localStorage.setItem(
+            "curso"+index,
+            this.dataset.estado
+        );
+
+    }
 
 });
